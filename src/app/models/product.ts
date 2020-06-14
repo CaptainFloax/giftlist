c746cbd4dfe388eb922d538f7cdf8b7c91e85311
Captain Floax <loic@studiofloax.fr>
2020-06-14 20:42:17 +0200

    feat(products) Init product models

---
+++ src/app/models/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+    id: number;
+    img: string;
+    name: string;
+    description: string;
+    price: number;
+    get: boolean;
+    cat: string;
+    link: string;
+    participate: boolean;
+}
